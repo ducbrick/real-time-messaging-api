8eeed6cac8e0c47ef53384789a9ca66879b615ff
ducbrick <ducbrick@gmail.com>
2025-04-27 16:04:57 +0700

    init

---
+++ src/main/java/com/ducbrick/real_time_messaging_api/RealTimeMessagingApiApplication.java
@@ -0,0 +1,13 @@
+package com.ducbrick.real_time_messaging_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeMessagingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeMessagingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ducbrick/real_time_messaging_api/RealTimeMessagingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ducbrick.real_time_messaging_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeMessagingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
