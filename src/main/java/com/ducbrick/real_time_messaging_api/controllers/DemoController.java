f4c21cbe01b2dc3ed573f41ee0f0d66abfdcb24c
ducbrick <ducbrick@gmail.com>
2025-04-27 16:15:20 +0700

    init

---
+++ src/main/java/com/ducbrick/real_time_messaging_api/controllers/DemoController.java
@@ -0,0 +1,20 @@
+package com.ducbrick.real_time_messaging_api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+	@GetMapping
+	public String get() {
+		return "Hello World";
+	}
+
+	@PostMapping
+	public String post() {
+		return "Hello World POST";
+	}
+}
