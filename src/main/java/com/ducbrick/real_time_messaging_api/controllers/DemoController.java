2a6fa1c26881f8e9e3106e9dfb7b249e8c6d845b
ducbrick <ducbrick@gmail.com>
2025-05-17 17:30:23 +0700

    update todo

--- src/main/java/com/ducbrick/real_time_messaging_api/controllers/DemoController.java
+++ src/main/java/com/ducbrick/real_time_messaging_api/controllers/DemoController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// TODO: Set this to TEST profile
 @RestController
 public class DemoController {
 
