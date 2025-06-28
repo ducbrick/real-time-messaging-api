562f317185c11fa9b0f768c777c70f0814c981bd
ducbrick <95757159+ducbrick@users.noreply.github.com>
2025-06-29 00:14:08 +0700

    update dto, proxy and test (#12)

--- src/main/java/com/ducbrick/real_time_messaging_api/dtos/AuthServerUsrInfo.java
+++ src/main/java/com/ducbrick/real_time_messaging_api/dtos/AuthServerUsrInfo.java
@@ -1,7 +1,7 @@
 package com.ducbrick.real_time_messaging_api.dtos;
 
 public record AuthServerUsrInfo(
-	String name,
+	String nickname,
 	String email
 ) {
 }
--- src/test/java/com/ducbrick/real_time_messaging_api/services/proxies/AuthServerProxyTest.java
+++ src/test/java/com/ducbrick/real_time_messaging_api/services/proxies/AuthServerProxyTest.java
@@ -1,9 +1,11 @@
 package com.ducbrick.real_time_messaging_api.services.proxies;
 
+import com.ducbrick.real_time_messaging_api.dtos.AuthServerUsrInfo;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -14,9 +16,20 @@ import static org.junit.jupiter.api.Assertions.*;
 class AuthServerProxyTest {
 	@Autowired private AuthServerProxy authServer;
 
+	@Value("${app.auth-server.access-token}") private String accessToken;
+
 	@DisplayName("Test constraints applied on params")
 	@Test
 	public void testConstraints() {
 		Assertions.assertThatThrownBy(() -> authServer.getUserInfo(null));
+	}
+
+	@DisplayName("Query for user info using access token")
+	@Test
+	public void queryUsrInfoByAccessToken() {
+		AuthServerUsrInfo usrInfo = authServer.getUserInfo(accessToken);
+
+		Assertions.assertThat(usrInfo.nickname()).isEqualTo("ducbrick");
+		Assertions.assertThat(usrInfo.email()).isEqualTo("ducbrick@gmail.com");
 	}
 }
