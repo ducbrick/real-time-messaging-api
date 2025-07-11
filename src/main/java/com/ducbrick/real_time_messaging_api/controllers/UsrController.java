a567bee1db9f1611c28f27dd747e042fc9c43b4e
ducbrick <ducbrick@gmail.com>
2025-07-12 00:45:02 +0700

    merged development into main

--- src/main/java/com/ducbrick/real_time_messaging_api/controllers/UsrController.java
+++ src/main/java/com/ducbrick/real_time_messaging_api/controllers/UsrController.java
@@ -4,6 +4,12 @@ import com.ducbrick.real_time_messaging_api.dtos.UsrInfoDto;
 import com.ducbrick.real_time_messaging_api.exceptions.NoSuchUsrE;
 import com.ducbrick.real_time_messaging_api.exceptions.UnauthenticatedE;
 import com.ducbrick.real_time_messaging_api.services.UsrService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -22,6 +28,35 @@ public class UsrController {
 
 	private final UsrService usrService;
 
+	@Operation(
+		summary = "Get information about a user"
+	)
+	@Parameter(
+			name = "id",
+			in = ParameterIn.QUERY,
+			description = "ID of the user to get information about",
+			schema = @Schema(type = "string")
+	)
+	@Parameter(
+			name = "sub",
+			in = ParameterIn.QUERY,
+			description = "OAuth2 subject of the user to get information about",
+			schema = @Schema(type = "string")
+	)
+	@Parameter(
+			name = "issuer",
+			in = ParameterIn.QUERY,
+			description = "OAuth2 issuer of the user to get information about",
+			schema = @Schema(type = "string")
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "Successfully retrieved user information"
+	)
+	@ApiResponse(
+			responseCode = "204",
+			description = "Specified user does not exist"
+	)
 	@GetMapping("/public/user/info")
 	@NotNull
 	@Valid
